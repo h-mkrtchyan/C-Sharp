0a0a889882061e27bc96b5beba44fbe076ee3356
h-mkrtchyan <hakobmkrtchyann@gmail.com>
2016-08-12 00:52:18 +0400

    PrinterEvents

---
+++ DelegatesEvents/DelegatesEvents/Printer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesEvents
+{
+    public class PrintingEventArgs : EventArgs
+    {
+        public int CurrentPage { get; set; }
+    }
+
+    public class PrintFailedEventArgs : EventArgs
+    {
+        public int RemainingPages { get; set; }
+    }
+    public class Printer
+    {
+        public string Name { get; private set; }
+
+        public Printer()
+        {
+            Name = Guid.NewGuid().ToString();
+        }
+
+        public Printer(string name)
+        {
+            Name = name;
+        }
+
+        public event EventHandler PrintStarted;
+        public event EventHandler PrintFinished;
+        public event EventHandler<PrintingEventArgs> Printing;
+        public event EventHandler<PrintFailedEventArgs> PrintFailed;
+
+        private void OnPrintStarted()
+        {
+            PrintStarted?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnPrintFinished()
+        {
+            PrintFinished?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnPrinting(int current)
+        {
+            var args = new PrintingEventArgs { CurrentPage = current };
+            Printing.Invoke(this, args);
+        }
+
+        private void OnPrintingFailed(int remaining)
+        {
+            var args = new PrintFailedEventArgs { RemainingPages = remaining };
+            PrintFailed?.Invoke(this, args);
+        }
+
+        public int PapersQountity { get; private set; }
+
+        public void AddPapers(int papersCount)
+        {
+            if(papersCount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(papersCount),
+                    $"The argument of {nameof(papersCount)} can't be negative");
+            }
+            PapersQountity += papersCount;
+        }
+
+        public void Print(int pagesCount)
+        {
+            if(pagesCount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pagesCount),
+                    $"The argument of {nameof(pagesCount)} should be greather than 0");
+            }
+
+            OnPrintStarted();
+
+            for (int i = 1; i <= pagesCount; i++)
+            {
+                if (PapersQountity != 0)
+                {
+                    OnPrinting(i);
+                    PapersQountity--;
+                }
+                else
+                {
+                    OnPrintingFailed(pagesCount - i + 1);
+                    break;
+                }
+            }
+
+            OnPrintFinished();
+        }
+    }
+}
---
+++ DelegatesEvents/DelegatesEvents/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();   
+        }
+
+        private static void Start()
+        {
+            var canon = new Printer();
+            canon.PrintStarted += Canon_PrintStarted;
+            canon.Printing += Canon_Printing;
+            canon.PrintFinished += Canon_PrintFinished;
+            canon.PrintFailed += Canon_PrintFailed;
+
+            var hp = new Printer();
+            hp.PrintStarted += Canon_PrintStarted;
+            hp.Printing += Canon_Printing;
+            hp.PrintFinished += Canon_PrintFinished;
+            hp.PrintFailed += Canon_PrintFailed;
+
+            try
+            {
+                canon.AddPapers(4);
+                canon.Print(2);
+                canon.Print(3);
+                Console.WriteLine("-------------------");
+                hp.AddPapers(20);
+                hp.Print(15);
+                hp.Print(20);
+            }
+            catch(ArgumentOutOfRangeException exception)
+            {
+                Console.WriteLine($"OOPS! Wrong argument {exception.ParamName}.{Environment.NewLine} Message: {exception.Message}");
+                throw new Exception("", exception);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+
+
+        private static void Canon_PrintStarted(object sender, EventArgs e)
+        {
+            var printer = sender as Printer;
+            Console.WriteLine($"{printer?.Name} print started...!");
+        }
+        private static void Canon_Printing(object sender, PrintingEventArgs e)
+        {
+            var printer = sender as Printer;
+            Console.WriteLine($"{printer?.Name} printing page N{e.CurrentPage}");
+        }
+        private static void Canon_PrintFinished(object sender, EventArgs e)
+        {
+            var printer = sender as Printer;
+            Console.WriteLine($"{ printer?.Name} successfully finished" );
+        }
+        
+        private static void Canon_PrintFailed(object sender, PrintFailedEventArgs e)
+        {
+            var printer = sender as Printer;
+            Console.WriteLine($"{printer?.Name} print failed and {e.RemainingPages} are remaining");
+        }
+    }
+}
